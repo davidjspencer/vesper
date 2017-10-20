7ded884fb92748ed22ca94071f7850527856ac6e
davidjspencer <davidjspencer@gmail.com>
2017-10-20 11:30:48 +0200

    menu as em, modal begins

--- js/menu.js
+++ js/menu.js
@@ -12,9 +12,17 @@
 
 function menu() {
 	var icon = document.getElementById('icon');
+	var body = document.getElementById('side-menu');
+
 	if (icon.className === "icon-three") {
-          icon.className += " active-three";
-      } else {
-          icon.className = "icon-three";
-      }
+    icon.className += " active-three";
+  } else {
+    icon.className = "icon-three";
+  }
+
+  if (body.className === "hide") {
+    body.className += " body-menu";
+  } else {
+    body.className = "hide";
+  }
 }
